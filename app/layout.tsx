7b1f8f4558339fccd61970f4e678c5633e73cf27
savbeM <mark.selihov2458@gmail.com>
2024-01-25 21:31:17 +0200

    Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import {inter} from "@/fonts/fonts";
+import {Header} from "@/components/Header";
 
 
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Header/>
+      {children}
+      </body>
     </html>
   );
 }
