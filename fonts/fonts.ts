fa2f0ce485285558366af5b3d5eb7aea4fc0b367
savbeM <mark.selihov2458@gmail.com>
2024-01-25 18:07:32 +0200

    init & fonts

---
+++ fonts/fonts.ts
@@ -0,0 +1,6 @@
+import {Eczar, Inter, Playfair_Display, Silkscreen} from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const chapter = Playfair_Display({subsets: ["latin"], weight: '400'})
+export const silkscreen = Silkscreen({subsets: ["latin"], weight: "700"})
+export const eczar =   Eczar({subsets: ["latin"], weight: ["600", "800"]})
